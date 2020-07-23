375bbe203427410b2624e5cb2ed152881e5a2a08
Daniel Kollner <dankollner@gmail.com>
2020-07-23 13:02:12 +0200

    react wired

---
+++ app/javascript/packs/firstapp/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+    const App = (props) => (
+  <div>Hello {props.name}!</div>
+)
+
+export default App;
---
+++ app/javascript/packs/firstapp/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import App from './components/app';
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  ReactDOM.render(<App name='Danika' />, document.getElementById('root'));
+})
